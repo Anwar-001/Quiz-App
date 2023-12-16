944cd2464552c329be9f62f8e5d5e2beea9d899b
Anwar-001 <130050966+Anwar-001@users.noreply.github.com>
2023-12-16 08:00:15 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,129 @@
-const gradientBox = document.querySelector(".gradient-box");
-const selectMenu = document.querySelector(".select-box select");
-const colorInputs = document.querySelectorAll(".colors input");
-const textarea = document.querySelector("textarea");
-const refreshBtn = document.querySelector(".refresh");
-const copyBtn = document.querySelector(".copy");
-const getRandomColor = () => {
-    // Generating a random color in hexadecimal format. Example: #5665E9
-    const randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
-    return `#${randomHex}`;
-}
-const generateGradient = (isRandom) => {
-    if(isRandom) { // If isRandom is true, update the colors inputs value with random color
-        colorInputs[0].value = getRandomColor();
-        colorInputs[1].value = getRandomColor();
+const questions = [
+
+    {
+        question: "Which is largest animal in the world? ",
+        answers:[
+            {text: "Shark", correct: false},
+            {text: "Blue Whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "Which is the smallest country in the world?",
+        answers:[
+            {text: "Vatican City", correct: true},
+            {text: "Bhutan", correct: false},
+            {text: "Nepal", correct: false},
+            {text: "India", correct: false},
+        ]
+    },
+    {
+        question: "Which is largest desert in the world?",
+        answers:[
+            {text: "Kalahari", correct: false},
+            {text: "Gobi", correct: false},
+            {text: "Sahar", correct: false},
+            {text: "Antarctica", correct: true},
+        ]
+    },
+    {
+        question: "Which is smallest continent in the world?",
+        answers:[
+            {text: "Asia", correct: false},
+            {text: "Australia", correct: true},
+            {text: "Arctic", correct: false},
+            {text: "Africa", correct: false},
+        ]
     }
-    // Creating a gradient string using the select menu value with color input values
-    const gradient = `linear-gradient(${selectMenu.value}, ${colorInputs[0].value}, ${colorInputs[1].value})`;
-    gradientBox.style.background = gradient;
-    textarea.value = `background: ${gradient};`;
+];
+
+const  questionElement = document.getElementById("question");
+const  answerButtons = document.getElementById("answer-buttons");
+const  nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score= 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+
 }
-const copyCode = () => {
-    // Copying textarea value and updating the copy button text
-    navigator.clipboard.writeText(textarea.value);
-    copyBtn.innerText = "Code Copied";
-    setTimeout(() => copyBtn.innerText = "Copy Code", 1600);
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer =>{
+        const button =  document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+
+    });
 }
-colorInputs.forEach(input => {
-    // Calling generateGradient function on each color input clicks
-    input.addEventListener("input", () => generateGradient(false));
-});
-selectMenu.addEventListener("change", () => generateGradient(false));
-refreshBtn.addEventListener("click", () => generateGradient(true));
-copyBtn.addEventListener("click", copyCode);
+
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+ function selectAnswer(e){
+    const selectBtn = e.target;
+    const isCorrect = selectBtn.dataset.correct ==="true";
+    if(isCorrect){
+        selectBtn.classList.add("correct");
+        score++;
+    }else{
+        selectBtn.classList.add("incorrect");
+    }
+
+    ///important
+    
+    Array.from(answerButtons.children).forEach(button =>{
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block"
+ }
+
+ function showScore(){
+    resetState();
+    questionElement.innerHTML =`Ypu scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+ }
+
+ function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+
+    }else{
+        showScore();
+    }
+ }
+
+ nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+ });
+
+startQuiz();
